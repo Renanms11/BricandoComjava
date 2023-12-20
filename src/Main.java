92c508da399811a1a63e96e8dcc5de462e3fd18b
RenanSena2023 <renansena20152proton.me>
2023-12-19 22:01:40 -0300

    bubble sort

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String args[]) {
+		Scanner read = new Scanner(System.in);
 
-		fibonacci(15);
-
+		int[] numeros = { 10, 2, 3, 45, 9, 4, 31, 1, 9, 482, };
+		int [] numerosOrdenados= ordenarLista(numeros);
+		for(int i =0 ; i < numerosOrdenados.length;i++) {
+			System.out.println(numerosOrdenados[i]);
+		}
+		 
+		
 	}
 
-	public static void fibonacci(int x) {
-		int aux, a = 0, b = 1;
+	public static int[] ordenarLista(int[] lista) {
+		int aux = 0;
+		for (int i = 0; i < lista.length - 1; i++) {
+			for (int ii = 0; ii < lista.length - 1; ii++) {
+				if (lista[ii] > lista[ii + 1]) {
+					aux = lista[ii + 1];
+					lista[ii + 1] = lista[ii];
+					lista[ii] = aux;
+				}
 
-		for (int i = 0; i < x; i++) {
-			System.out.print(a + " ");
-			aux = b;
-			b += a;
-			a = aux;
-
+			}
 		}
+		return lista;
 
 	}
 
