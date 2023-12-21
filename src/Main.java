862899997364336a2f72d6697e8b9d1b9aeae180
RenanSena2023 <renansena20152proton.me>
2023-12-21 15:10:14 -0300

    algoritmo selectionSort

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,37 @@ public class Main {
 	public static void main(String args[]) {
 		Locale.setDefault(Locale.US);
 		Scanner read = new Scanner(System.in);
-		
-		
-		imprimeFibonacciRecursivo(10);
+
+		int[] listaDesordenada = { 5, 2, 3, 1, 48, 4, 6, 5, 9, 11 };
+
+		// int [] listaDesordenada = new int[10];
+		/*
+		 * for(int i = 0 ; i<listaDesordenada.length ; i++) {
+		 * System.out.println("digite o primeiro valor :"); listaDesordenada[i]=
+		 * read.nextInt(); }
+		 */
+
+		selectionSort(listaDesordenada);
+
+		for (int i = 0; i < listaDesordenada.length ; i++) {
+			System.out.print(listaDesordenada[i] + " ");
+
+		}
 
 	}
 
-	
-	
-	  public static void imprimeFibonacciRecursivo(int n) {
-	        for (int i = 0; i <= n; i++) {
-	            System.out.print(fibonacciRecursivo(i) + " ");
-	        }
-	    }
-
-	    public static int fibonacciRecursivo(int n) {
-	        if (n <= 1) {
-	            return n;
-	        } else {
-	            return fibonacciRecursivo(n - 1) + fibonacciRecursivo(n - 2);
-	        }
-	    }
-	
+	public static void selectionSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int index = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[index]) {
+					index = j;// searching for lowest index
+				}
+			}
+			int smallerNumber = arr[index];
+			arr[index] = arr[i];
+			arr[i] = smallerNumber;
+		}
+	}
 
 }
