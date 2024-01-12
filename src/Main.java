ea0bf0d822b6712bfcb8ca33113eaf948105d47e
RenanSena2023 <renansena20152proton.me>
2024-01-12 00:49:48 -0300

    Threads  e classe anonima com runnable

--- src/Main.java
+++ src/Main.java
@@ -10,61 +10,32 @@ import javax.imageio.ImageIO;
 
 public class Main {
 
-	public static void main(String args[]) throws IOException   {
-		 Locale.setDefault(Locale.US);
-		 Scanner read = new Scanner(System.in);
-		 String path = "res/dog.png";
-		 File imagem = new File(path);
-		 BufferedImage bimagem =  ImageIO.read(imagem);
-		 
-		    BufferedImage imagemCinza = converterParaCinza(bimagem);
-
-            // Salvar a nova imagem em tons de cinza
-            File arquivoImagemCinza = new File("res/imagem_cinza.jpg");
-            ImageIO.write(imagemCinza, "jpg", arquivoImagemCinza);
-	
-	
+	public static void main(String args[]) throws IOException {
+		Locale.setDefault(Locale.US);
+		Scanner read = new Scanner(System.in);
+		
+		Thread t = new Thread("number 1")  {
+			public void run() {
+				System.out.print("ola");
+			}
+		};
+		Thread t2 = new Thread(thread2);
+		t.start();
+		t2.start();
 
 	}
 	
 	
-	 private static BufferedImage converterParaCinza(BufferedImage imagem) {
-	        int largura = imagem.getWidth();
-	        int altura = imagem.getHeight();
+	
+	public static Runnable thread2 = new Runnable() {
 
-	        // Criar uma nova imagem em tons de cinza
-	        BufferedImage imagemCinza = new BufferedImage(largura, altura, BufferedImage.TYPE_INT_RGB);
+		@Override
+		public void run() {
+			System.out.print(" mundo ");
+			
+			
+		}
+		
+	};
 
-	        // Iterar sobre todos os pixels da imagem original
-	        for (int y = 0; y < altura; y++) {
-	            for (int x = 0; x < largura; x++) {
-	                // Obter o valor RGB do pixel
-	                int rgb = imagem.getRGB(x, y);
-
-	                // Calcular a média dos componentes de cor
-	                int media = calcularMedia(rgb);
-
-	                // Criar novo valor RGB usando a média
-	                int novoRgb = (media << 16) | (media << 8) | media;
-
-	                // Definir o novo valor RGB na imagem em tons de cinza
-	                imagemCinza.setRGB(x, y, novoRgb);
-	            }
-	        }
-
-	        return imagemCinza;
-	    }
-
-	    // Método auxiliar para calcular a média dos componentes de cor
-	    private static int calcularMedia(int rgb) {
-	        int red = (rgb >> 16) & 0xFF;
-	        int green = (rgb >> 8) & 0xFF;
-	        int blue = rgb & 0xFF;
-
-	        // Calcular a média dos componentes de cor
-	        return (red + green + blue) / 3;
-	    }
-	}
-
-
-
+}
