d2811409e1aaaffa1cbc4ff28b4766bd9e2be193
RenanSena2023 <renansena20152proton.me>
2024-01-25 02:50:06 -0300

    classe generica que extends comparable, que compara com o probrio objeto

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,19 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 class Main {
-	static int tempo = 0, i;
 
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner read = new Scanner(System.in);
-		Timer timer = new Timer();
-
-		TimerTask tt = new TimerTask() {
-
-			@Override
-			public void run() {
-				i++;
-				System.out.println("ola mundo " + i);
-				tempo++;
-				if (tempo == 10) {
-					cancel();
-					timer.cancel();
-					
-				}
-
-			}
-		};
-
-
-		timer.scheduleAtFixedRate(tt, 500, 1000);
+		
+		
+		MinhaClasseGeneriaca<String, Integer> minhag = 
+				new MinhaClasseGeneriaca<String, Integer>("Renan miranda Sena",35);
+		
+		
+		System.out.println(minhag.getOjeto() + " " + minhag.getOjeto2());
+				
 	
-
-		System.out.println("fim do programa !!!");
-
 	}
 }
--- src/MinhaClasseGeneriaca.java
+++ src/MinhaClasseGeneriaca.java
@@ -1,15 +1,18 @@
 import java.util.List;
 
-public class MinhaClasseGeneriaca <T>{
+public class MinhaClasseGeneriaca <T,K>{
 	
 	private T objeto = null;
+	private K objeto2 =null;
 	private List<T> lista = null;
 	
 	public MinhaClasseGeneriaca(){
 	}
 	
-	public MinhaClasseGeneriaca(T objeto){
+	public MinhaClasseGeneriaca(T objeto,K objeto2){
 		this.objeto = objeto;
+		this.objeto2 = objeto2;
+
 		
 	}
 	
@@ -19,15 +22,24 @@ public class MinhaClasseGeneriaca <T>{
 	public T getOjeto() {
 		return objeto;
 	}
+	public K getOjeto2() {
+		return objeto2;
+	}
 	
 	
 	public void showTipo() {
 		System.out.println("T tipo é " + this.objeto.getClass().getName());
+		System.out.println("K tipo é " + this.objeto2.getClass().getName());
 	}
 	
 	public void setObjeto(T objetoNovo) {
 	
 		this.objeto = objetoNovo;
+	}
+	
+	public void setObjeto2(K objetoNovo2) {
+		
+		this.objeto2 = objetoNovo2;
 	}
 
 }
