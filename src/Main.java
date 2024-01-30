47610206a7ec78a43741e7a25baf088b39c09ab4
RenanSena2023 <renansena20152proton.me>
2024-01-30 02:42:54 -0300

    brincando com Stream

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class Main {
 
@@ -16,6 +18,7 @@ class Main {
 		Locale.setDefault(Locale.US);
 		Scanner read = new Scanner(System.in);
 
+		Function<Aluno,String> func =(p)  -> p.getName().toUpperCase();
 		Comparator<Aluno> comp = (aluno1,aluno2)->aluno1.getName().toUpperCase().compareTo(aluno2.getName().toUpperCase());
 		Predicate<Aluno> pred = (aluno) -> aluno.getMedia()>7;
 		Consumer<Aluno> cons =  aluno -> {
@@ -37,14 +40,26 @@ class Main {
 		
 		// foreach metodo da lista
 		//referencia a metodo
-		lista.forEach(System.out::println);
-		System.out.println("****************************************");
+		//lista.forEach(System.out::println);
+		//System.out.println("****************************************");
 		
 		lista.stream().map(x -> x+=10).collect(Collectors.toList());
 		
-		lista.forEach(System.out:: println);
+		//lista.forEach(System.out:: println);
 		
+		System.out.println("*******************************************************");
 
+		
+		List<Integer> lista2 = Arrays.asList(10,28,33,10,46,58);
+		Stream<Integer> str = lista2.stream().map(x -> x*10);
+		
+		System.out.println(Arrays.toString(str.toArray()));
+		Stream<String>str2 = Stream.of("Renan","Katianny","Francimar","Francisco de Assis","Kamila");
+		System.out.println(Arrays.toString(str2.toArray()));
+
+		Stream<Integer> str3 = Stream.iterate(0, x-> x+2).limit(15);
+		
+		System.out.println(Arrays.toString(str3.toArray()));
 	}
 
 }
